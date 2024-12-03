d1bbec9289f16bc5d84e1110fe2d75fdae3cfbd9
Misty De Meo <mistydemeo@gmail.com>
2024-12-03 15:37:37 -0800

    Remove two unused commands

--- cmd/brew-no-arm-bottles.rb
+++
@@ -1,8 +0,0 @@
-unbottled = Tap.new("homebrew", "core").formula_files.sort.map do |f|
-  data = f.read
-  if !data.include?("arm64_big_sur") && !data.include?("bottle :unneeded")
-    f.basename.to_s.sub(/\.rb$/, "")
-  end
-end.compact
-
-puts unbottled
